66cdc9f0663d39820bdaf7c7110ad08a688f5aca
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-07-08 22:35:48 -0400

    mejorar interfaz de usuario

--- main.js
+++ main.js
@@ -11,26 +11,56 @@ document.querySelector('#app').innerHTML = /*html*/ `
         <button id="js-generar-planillas">Generar Planillas</button>
         <button id="js-generar-planillas-pasajes">Generar Planillas Pasajes</button>
 
-        <embed
-            id="embed"
-            src=""
-            type="application/pdf"
-            width="100%"
-            height="800px"
-        />
+        <div class="container">
+            <div id="js-spinner" class="spinner"></div>
+
+            <embed
+                id="js-embed"
+                src=""
+                type="application/pdf"
+                width="100%"
+                height="800px"
+            />
+        </div>
+
     </div>
 `;
 
-const $embed = document.querySelector('#embed');
+const $embed = document.querySelector('#js-embed');
+const $spinner = document.querySelector('#js-spinner');
 
-document.querySelector('#js-generar-planillas').onclick = () => {
-    $embed.src = generarPlanillas([planillaData, planillaData, planillaData]);
-};
+$spinner.style.display = 'none';
+$embed.style.display = 'none';
 
-document.querySelector('#js-generar-planillas-pasajes').onclick = () => {
-    $embed.src = generarPlanillasPasajes([
-        planillaPasajesData,
-        planillaPasajesData,
-        planillaPasajesData,
-    ]);
+document
+    .querySelector('#js-generar-planillas')
+    .addEventListener('click', () => {
+        eventoClick(() => {
+            return generarPlanillas([planillaData, planillaData, planillaData]);
+        });
+    });
+
+document
+    .querySelector('#js-generar-planillas-pasajes')
+    .addEventListener('click', () => {
+        eventoClick(() => {
+            return generarPlanillasPasajes([
+                planillaPasajesData,
+                planillaPasajesData,
+                planillaPasajesData,
+            ]);
+        });
+    });
+
+const eventoClick = (fn) => {
+    $embed.style.display = 'none';
+    $spinner.style.display = 'block';
+
+    const srcEmbed = fn();
+
+    setTimeout(() => {
+        $spinner.style.display = 'none';
+        $embed.style.display = 'block';
+        $embed.src = srcEmbed;
+    }, 2000);
 };
